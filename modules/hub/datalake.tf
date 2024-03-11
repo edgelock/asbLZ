21cc3e003a7a1d76ed4fad365715407a34343f05
Jhante Charles <jcharles@3cloudsolutions.com>
2024-03-11 15:40:00 -0400

    It works

---
+++ modules/hub/datalake.tf
@@ -0,0 +1,20 @@
+resource "azurerm_storage_account" "example" {
+  name                     = "${var.env}${var.app_name}st"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
+  allow_nested_items_to_be_public = false
+  tags = var.resource_tags
+}
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "example" {
+  name               = "dl${var.app_name}${var.env}"
+  storage_account_id = azurerm_storage_account.example.id
+
+  properties = {
+    hello = "aGVsbG8="
+  }
+}
---
+++ modules/hub/peering.tf
@@ -0,0 +1,13 @@
+resource "azurerm_virtual_network_peering" "example-1" {
+  name                      = "peerHubToDev"
+  resource_group_name       = azurerm_resource_group.hub.name
+  virtual_network_name      = azurerm_virtual_network.virtual_network.name
+  remote_virtual_network_id = "/subscriptions/51325363-3635-412f-b99f-26238c60a8ba/resourceGroups/inv-dev-uan-vnet-rg/providers/Microsoft.Network/virtualNetworks/inv-dev-uan-vnet"
+}
+
+resource "azurerm_virtual_network_peering" "example_2" {
+  name                      = "peerHubToPrd"
+  resource_group_name       = azurerm_resource_group.hub.name
+  virtual_network_name      = azurerm_virtual_network.virtual_network.name
+  remote_virtual_network_id = "/subscriptions/6b55ba09-7467-473f-81b8-d751c3f6fb87/resourceGroups/inv-prd-uan-vnet-rg/providers/Microsoft.Network/virtualNetworks/inv-prd-uan-vnet"
+}
