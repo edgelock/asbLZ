c0ee4577af7fd5310ae91cb875f262792e857054
Jhante Charles <jcharles@3cloudsolutions.com>
2024-03-14 11:15:54 -0400

    Working damn it

--- modules/prd/peering.tf
+++ modules/prd/peering.tf
@@ -1,6 +1,6 @@
-resource "azurerm_virtual_network_peering" "example_1" {
-  name                      = "prdToHubPeering"
-  resource_group_name       = azurerm_resource_group.prd.name
-  virtual_network_name      = azurerm_virtual_network.virtual_network.name
+resource "azurerm_virtual_network_peering" "prd" {
+  name                      = "peerPrdVnetToHubVnet"
+  resource_group_name       = var.resource_group_name
+  virtual_network_name      = "inv-prd-uan-vnet"
   remote_virtual_network_id = "/subscriptions/57954a41-f93e-41af-bbc1-6e34424e09c6/resourceGroups/hub-uan-vnet-rg/providers/Microsoft.Network/virtualNetworks/hub-uan-vnet"
 }
